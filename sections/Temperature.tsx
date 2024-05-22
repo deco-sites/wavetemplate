a9b09a39e1ab2653ff94895580da31e52ecb1f57
sergio-franca-wave <139240473+sergio-franca-wave@users.noreply.github.com>
2024-05-22 19:55:52 -0300

    Release sergio environment with 1 changes made by Sérgio Junior

--- sections/Temperature.tsx
+++ sections/Temperature.tsx
@@ -1,25 +1,24 @@
 import type { Temperature } from "apps/weather/loaders/temperature.ts";
 
 export interface Props {
-    /**
-     * @title Temperatura
-     */
-    temperature?: Temperature | null;
-    /**
-     * @title Localização
-     */
-    location?: string;
-  }
+  /**
+   * @title Temperatura
+   */
+  temperature?: Temperature | null;
+  /**
+   * @title Localização
+   */
+  location?: string;
+}
 
-  const Temperature = ({ temperature, location }: Props) => {
+const Temperature = ({ temperature, location }: Props) => {
+  return (
+    <>
+      <div class="px-4 mt-2 fixed p-1 bottom-3 left-3 bg-black text-white">
+        <span class="text-lg italic">{location}, {temperature?.celsius}°C</span>
+      </div>
+    </>
+  );
+};
 
-    return (
-      <>
-        <div class="px-4 mt-2 fixed p-1 rounded-lg bottom-3 left-3 bg-gray-600 text-white shadow-sm">
-          <span class="text-lg italic">{location}, {temperature?.celsius}°C</span>
-        </div>
-      </>
-    );
-  }
-  
-  export default Temperature;
+export default Temperature;
